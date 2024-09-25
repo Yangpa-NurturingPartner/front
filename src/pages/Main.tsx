46080ec6ea05975173690019a6118048d996152a
jeongmin choi <jeongmin0521@naver.com>
2024-09-25 14:47:47 +0900

    임시저장

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,16 +1,46 @@
-import React from 'react';
-import {useMediaQuery} from "react-responsive";
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom'; 
+import { useMediaQuery } from "react-responsive";
 import SearchPart from "../components/desktop/main/SearchPart";
-import "../css/mainCsss.scss"
+import "../css/mainCsss.scss";
 import SearchNavigate from "../components/desktop/main/SearchNavigate";
+import axios from "axios";
 
 const Main: React.FC = () => {
-    const isPortrait = useMediaQuery({query: '(orientation: portrait)'});
+    const isPortrait = useMediaQuery({ query: '(orientation: portrait)' });
+    const [sessionId, setSessionId] = useState<string | null>(null);
+    const [query, setQuery] = useState<string>(''); 
+    const navigate = useNavigate(); 
+
+    useEffect(() => {
+        const startNewChat = async () => {
+            const response = await axios.post('http://localhost:8080/chat/start-new-chat');
+            setSessionId(response.data.session_id);
+        };
+
+        startNewChat(); // 컴포넌트가 마운트될 때 새로운 채팅 시작
+    }, []);
+
+    //질문 제출
+    const handleMainQuery = async (question: string) => { 
+        console.log("질문 제출:", question);
+        setQuery(question);
+        try {
+            const response = await axios.post('http://localhost:8080/chat/message', {
+                session_id: sessionId,
+                chat_detail: { query: question }
+            });
+            navigate('/chat');
+            console.log("응답:", response.data); // 응답 로그 추가
+        } catch (error) {
+            console.error("질문 제출 오류:", error);
+        }
+    };
 
     return (
         <div className={"pc-mainpage"}>
-            <SearchPart/>
-            <SearchNavigate/>
+            <SearchPart MainQuery={handleMainQuery} />
+            <SearchNavigate />
         </div>
     );
 };
